b1c3424c0cc7b39cadf7b9ced6679976e52e393d
jpaquim <joaopaquim@gmail.com>
2022-05-18 22:47:34 +0100

    Fix keyword, symbol constructors and assoc duplicate keys

--- impls/zig-mal/src/reader.zig
+++ impls/zig-mal/src/reader.zig
@@ -166,7 +166,7 @@ fn read_atom(allocator: Allocator, reader: *Reader) !*MalType {
         else if (token[0] == '"') MalType{
             .string = try replaceEscapeSequences(allocator, token[1 .. token.len - 1]),
         } else if (token[0] == ':') MalType{
-            .keyword = try std.mem.concat(allocator, u8, &.{ "ʞ", token[1..] }),
+            .keyword = try MalType.addKeywordPrefix(allocator, token[1..]),
         } else if (std.fmt.parseInt(i64, token, 10)) |int| MalType{
             .number = int,
         } else |_| MalType{
